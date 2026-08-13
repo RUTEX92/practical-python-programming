050b30b4a6065440d76a46e71cdcc965b7238fe2
RUTEX92 <ruthsanda92@mau.edu.ng>
2026-08-13 11:15:45 +0100

    Add decorators exercise

---
+++ intermediate/08_decorators/decorators.py
@@ -0,0 +1,11 @@
+import time
+def timed(fn):
+    def wrapper(*a, **kw):
+        t = time.time()
+        out = fn(*a, **kw)
+        print(fn.__name__, time.time()-t)
+        return out
+    return wrapper
+
+@timed
+def train_model(): ...
